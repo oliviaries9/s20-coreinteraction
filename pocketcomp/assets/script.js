2e43aad2110c52aa47bda61c9d1054edfe3fb2fe
oliviaries9 <rieso777@newschool.edu>
2020-02-28 16:02:50 -0500

    multiple

--- pocketcomp/assets/script.js
+++ pocketcomp/assets/script.js
@@ -1,9 +1,52 @@
-var greeting = "hello!"
 
-console.log(greeting)
+// example var myRootDiv = document.getElementById("container");
 
-	
 
-var root = document.getElementById("column1")
+//for (var i = 0; i < 50; i++) {
 
-var root = 
+//var firstVariable = 'replace me wiht some css';
+//var myModule = `
+//<div class="myClass" style="${ firstVariable }">
+//<div class="diagonalLine" style="${ firstVariable }"></div>
+//<div class="oval" style="${ firstVariable }"></div> </div>;
+
+// myRootDiv.InsertAdjacentHTML('beforeend', myModule)}
+
+
+/* What do I want to happen? 
+ 
+ I want the background color to change 
+
+
+
+
+*/ 
+
+
+
+//var greeting = "hello!"
+
+//console.log(greeting)
+
+var yellow = #FFFF00;
+
+var root = document.getElementById("column1") 
+
+for (var i = 0; i<50; i++) {
+
+var column1 = BackgroundColor;
+
+var BackgroundColor = yellow;
+
+
+    var myModule = `
+
+        <div class="content1" style="${ root }">
+            <div class="content1" style="${ root }"></div>
+            <div class="content1" style="${ root }"></div>
+        </div>
+
+    `;
+
+    root.InsertAdjacentHTML('beforeend', myModule)
+}
